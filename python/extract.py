b8e2c5a81e7f796129e9d57a22ee9355dba6271e
Max Kaufmann <jkaufma@us.ibm.com>
2016-02-19 10:30:54 -0600

    Fixed bug in extract.py

--- python/extract.py
+++ python/extract.py
@@ -11,17 +11,12 @@ from collections import defaultdict
 # classesreportfile - Reports the number of unique classes. Each line is a class, with the primary question as a key
 
 #TODO # o
-def extract(gttsnapshotxml, outputfile, numquestions, classesreportfile):
+def extract(gttsnapshotxml, csvfile):
 
-    print("No. of training instances requested : ", numquestions)
 
     root = ET.parse(gttsnapshotxml).getroot()
-    csvFile = open(outputfile, 'w')
-    dictionaryFlie = open(classesreportfile, 'w')
-    questionFile = open("questionfile.csv",'w')
 
-    csvWriter = csv.writer(csvFile, delimiter=',')
-    questionFileWriter = csv.writer(questionFile,delimiter=',')
+    csvWriter = csv.writer(open(csvfile, 'w'), delimiter=',')
 
     tCount = 0
     count = 0
@@ -34,7 +29,6 @@ def extract(gttsnapshotxml, outputfile, numquestions, classesreportfile):
 	
         id = question.find('id').text if question.find('id') is not None else ""
         value = question.find('value').text if question.find('value') is not None else ""
-
         questionText = value.lstrip().rstrip()
 
         # Get primary question PAU
@@ -46,21 +40,12 @@ def extract(gttsnapshotxml, outputfile, numquestions, classesreportfile):
 
         # Check for valid question, has mapped question section and not PAU - This means secondary question
         if questionText != "" and predefinedAnswerUnit == "" and mappedQuestion != "":
-            parentQuestionPau = ""
             parentQuestionPau = mappedQuestion.find('predefinedAnswerUnit') if mappedQuestion.find(
                 'predefinedAnswerUnit') is not None else ""
 
+            	
+
             mappedQuestionText = mappedQuestion.find("value").text
-
-            #We only keep the question if we haven't seen it before. We don't want to create mulitple lines in the CSV for duplicates. This will just confuse the NLC
-            if questionDictionary.get(mappedQuestionText) is None or questionDictionary.get(mappedQuestionText) == "":
-                csvWriter.writerow([mappedQuestionText.encode('utf-8'), parentQuestionPau.text])
-                questionDictionary.update({mappedQuestionText: parentQuestionPau.text})
-                classesDict.update({parentQuestionPau.text: mappedQuestionText})
-                classCount += 1
-                count += 1
-                continue
-
 
             if parentQuestionPau != "":
                 if questionDictionary.get(questionText) is None or questionDictionary.get(questionText) == "":
@@ -102,25 +87,13 @@ def extract(gttsnapshotxml, outputfile, numquestions, classesreportfile):
     for k,v in count_map.iteritems():
         print len(v), "classes with", k, "items"
 
-    for item in classesDict.items():
-        dictionaryFlie.write(str(item))
-        dictionaryFlie.write('\n')
 
-    for item1 in questionDictionary.items():
-        questionFile.write(str(item1))
-        questionFile.write('\n')
-
-    csvFile.close()
-    dictionaryFlie.close()
-    questionFile.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("gttsnapshotxml", help="Ground Truth Snapshot XML File")
     parser.add_argument("outputfile", help="Output CSV File ")
-    parser.add_argument("numquestions", help="No. of questions requested, as integer",type=int)
-    parser.add_argument("classesreportfile", help="Classes Report CSV, ture or false")
     args = parser.parse_args()
-    extract(args.gttsnapshotxml, args.outputfile, args.numquestions, args.classesreportfile)
+    extract(args.gttsnapshotxml, args.outputfile)
 
-
+	
